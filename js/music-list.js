a6bfd8e7298de1ebcce58b3a199a2b78ff5f8721
Efeakpor <98009745+Efe-T@users.noreply.github.com>
2022-02-21 13:02:18 +0100

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,13 @@
+let allMusic = [{
+        name: "Sabrina Carpenter - Fast track",
+        artist: "Sabrina Carpenter",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Best of Naira Marley",
+        artist: "Dj Kaywise",
+        img: "music-2",
+        src: "music-2"
+    }
+]
---
+++ js/script.js
@@ -0,0 +1,4 @@
+$('.menu-btn').click(function () {
+    $('.container .menu-list').toggleClass("active");
+    $('.menu-btn i').toggleClass("active");
+});
