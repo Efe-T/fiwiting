44e2aa91c49afc1ff638a7dae8245241e6611198
Efeakpor <98009745+Efe-T@users.noreply.github.com>
2022-02-21 17:14:27 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$('.menu-btn').click(function () {
+    $('.container .menu-list').toggleClass("active");
+    $('.menu-btn i').toggleClass("active");
+});
+
+$('.home-btn').click(function () {
+    $('.container .menu-list').toggleClass("active");
+    $('.menu-btn i').toggleClass("active");
+});
